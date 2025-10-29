67a1478a411d11c3034af50c02ea1575c91c1343
Arun Parihar <parihararunrajput@gmail.com>
2025-10-29 10:31:07 +0000

    Conditional statements

--- UNIT_2/Conditional_Statements/1st_if.cpp
+++ UNIT_2/Conditional_Statements/1st_if.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int age = 18;
-    if(age <= 18){
+    int age = 12;
+    if(age >= 18){
         cout<<"You are eligble for voting ";
+    }
+    else if(age > 100){
+        cout<<"Person is Dead ";
+    }
+    else{
+        cout<<"You are under age";
     }
     return 0;
 }
