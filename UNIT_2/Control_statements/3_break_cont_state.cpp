7a71afbbbd8add66c575ac7dbe872019346baf6b
Arun2375 <parihararunrajput@gmail.com>
2025-10-31 01:15:29 +0530

    break & continue

---
+++ UNIT_2/Control_statements/3_break_cont_state.cpp
@@ -0,0 +1,20 @@
+//The break statement is used to immediately terminate a loop or a switch statement.
+//When the break statement is executed, control is transferred to the statement just
+// after the loop or switch.
+//It stops the loop completely when a specific condition is met.
+
+#include <iostream>
+using namespace std;
+/*int main(){
+    for (int i = 1; i <= 6; i++){
+        if (i == 4)
+        break;
+        cout<<i<<" ";
+    }
+    return 0;
+}
+    */
+
+//CONTINUE STATEMENT
+//It doesn't stop the loop 
+//It only skips the current iteration when the condition mets-> next iteration
