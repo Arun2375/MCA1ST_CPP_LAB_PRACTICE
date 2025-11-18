87af7ac38eb91161a368a621d62113a5308ab4a3
Arun2375 <parihararunrajput@gmail.com>
2025-11-19 01:38:13 +0530

    cpp

---
+++ tutorial/Basics/6_I_O.cpp
@@ -0,0 +1,14 @@
+//Input / Output streams
+// cin keyword used for input -- >> extraction operator 
+//cout keyword used for output -- << insertion operator
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a Number: ";
+    cin >> num;
+    cout << num;
+    return 0;
+}
