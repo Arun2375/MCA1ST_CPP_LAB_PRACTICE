71bc5dfaf59202bc266fda20676068192c4369e9
Arun2375 <parihararunrajput@gmail.com>
2025-11-18 23:06:29 +0530

    practice

--- UNIT_3/PRACTICE/3_variable_datatypes.cpp
+++ UNIT_3/PRACTICE/3_variable_datatypes.cpp
@@ -28,7 +28,7 @@ VARIABLE SCOPE
 
 #include <iostream>
 using namespace std;
-int a = 5;         // global variable 
+/*int a = 5;         // global variable 
 void func()
 {
     cout<<a<<endl;
@@ -39,4 +39,42 @@ int main()
     cout<<a<<endl;
     func();
     return 0;
+}
+*/
+
+//DATATYPES
+//DT define the type of data a variable can hold.
+
+// TYPES 
+
+// Built in DT
+// predefined for a language and could be used directly by the programmer
+// user-defined data types
+// These data types are defined by the user itself.
+
+// Examples are: class, struct, union, enum
+
+// Derived data types
+// These data types are derived from the primitive built-in data types.
+
+// Examples are: array, pointer, function
+
+// Some of the popular built-in data types and their applications are:
+
+// Data Type	Size	              Description
+// int	  2 or 4 bytes	           Stores whole numbers, without decimals
+// float	4 bytes	               Stores fractional numbers, containing one or more decimals. They require 4 bytes of memory space.
+// double	8 bytes         	   Stores fractional numbers, containing one or more decimals. They require 8 bytes of memory space.
+// char	1 byte	                   Stores a single character/letter/number, or ASCII values
+// boolean	1 byte	               Stores true or false
+
+//C++ CONSTANTS
+// Constants are unchangeable; when a constant variable is initialized in a program, 
+// its value cannot be changed afterwards.
+
+int main ()
+{
+    const float PI = 3.14;
+    cout<<"The value of PI is "<<PI<<endl;  
+    PI = 3.00; //error, since changing a const variable is not allowed.
 }
