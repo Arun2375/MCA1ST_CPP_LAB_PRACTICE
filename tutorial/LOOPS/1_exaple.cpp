32d561c07b04bb2239f48c7b16c88ad3c5563100
Arun2375 <parihararunrajput@gmail.com>
2025-11-21 16:26:10 +0530

    cpp

---
+++ tutorial/LOOPS/1_exaple.cpp
@@ -0,0 +1,9 @@
+//printing number from 1 to 5
+#include <iostream> 
+using namespace std;
+int main(){
+    for(int i = 1; i <= 5; i++){
+        cout <<i<<" ";
+    }
+    return 0;
+}
---
+++ tutorial/LOOPS/2_example.cpp
@@ -0,0 +1,11 @@
+// Program to display a text 5 times
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 1; i <= 5; i++){
+        cout<<"Arun Parihar!\n";
+    }
+    return 0;
+}
